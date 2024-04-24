030da048adb953703fd8a630fde8e3eb979d3599
Meer-001 <ahmer.rehman001@gmail.com>
2024-04-25 03:41:51 +0500

    publishing to git

---
+++ index.ts
@@ -0,0 +1,48 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let toDo = []
+let name = []
+let user = await inquirer.prompt(
+    [
+        {
+                name:"userName",
+                type:"input",
+                message: "Enter your name here"
+        
+            
+
+        }
+    ]
+);
+
+let condition = true
+while (condition) {
+    
+let addTask = await inquirer.prompt([
+    {
+        name:"todos",
+        type:"input",
+        message: "Do you want to add anything on your to-do list?"
+
+    },
+
+    {
+
+
+        name:"addMore",
+        type:"confirm",
+        message:"Do you want to Add more?",
+
+
+    }
+]
+);
+
+toDo.push(addTask.todos)
+name.push(user.userName)
+condition = addTask.addMore
+console.log("user:",user.userName ,"\n" ,"item", toDo);
+}; 
+console.log("\n","Created by - Ahmer Shaikh","\n");
